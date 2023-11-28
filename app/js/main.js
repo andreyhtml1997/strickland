8c7935fd30ee5f04f4b9de5e7f51ced299f4978f
andreyhtml1997 <andreyhtml1997@gmail.com>
2023-11-28 17:36:05 +0200

    html 90%, css 20%

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,11 @@
 window.onload = function () {
   Fancybox.bind("[data-fancybox]", {
     // Your custom options
+    closeButton: false,
+    dragToClose: false,
+    Thumbs: false,
   });
+
   //Preloader
     /* document.querySelector('.preloader').classList.add('hidden');
     // document.querySelector('html').classList.remove('overflow-hidden');
@@ -17,7 +21,12 @@ window.onload = function () {
     }));
     
 
-
+    const swiper = new Swiper(".bio-carousel", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
 }
 
 
@@ -33,8 +42,64 @@ window.onscroll = function(e) {
 	} else {
 		document.querySelector('.toTop').classList.remove('hidden');
 	}
-  
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let curs = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+    curs.style.left = x - 25 + "px";
+    curs.style.top = y - 25 + "px";
+});
+
+let images = document.querySelectorAll(".books--main .books__el");
+let servicesImages = "url('img/svg/arrow-right-white-circle.svg')";
+
+images.forEach((image, i) => {
+    image.addEventListener("mouseover", (e) => {
+        curs.classList.add("cursor-image-show");
+        curs.style.backgroundImage = servicesImages;
+    });
+    image.addEventListener("mouseleave", (e) => {
+        curs.classList.remove("cursor-image-show");
+        curs.style.backgroundImage = "none";
+    });
+});
+
+
+
+const button_collapses = document.querySelectorAll('.button-collapse');
+if(button_collapses) {
+  button_collapses.forEach(el => el.querySelector('.button__text').style.width = `0px`);
+  button_collapses.forEach(el => el.addEventListener('mouseover', function(){
+    const text_width = el.querySelector('.button__text-inner').offsetWidth;
+    el.querySelector('.button__text').style.width = `${text_width + 16}px`
+  }));
+
+  button_collapses.forEach(el => el.addEventListener('mouseleave', function(){
+    el.querySelector('.button__text').style.width = `0px`
+  }));
+}
+
+
+const events_list = document.querySelector('.events-list');
+events_list.addEventListener('mouseover', function(e) {
+  e.currentTarget.classList.add('active');
+});
+events_list.addEventListener('mouseleave', function(e) {
+  e.currentTarget.classList.remove('active');
+});
